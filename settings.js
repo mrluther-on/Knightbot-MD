bf6f3ec745a36b5abfac1d30efe0ec47b16af869
MR LUTHER <lutherofficial009@gmail.com>
2025-05-18 22:25:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  packname: '꯭ࠣ🇸𝖚𝘣𝖆𝘙𝖚𝆹꯭𝅥⃝👑⎯̽',
+  author: '🫶🏻',
+  botName: "LUTHER MD",
+  botOwner: '꯭ࠣ🇸𝖚𝘣𝖆𝘙𝖚𝆹꯭𝅥⃝👑⎯̽', // Your name
+  ownerNumber: '919614477116', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
